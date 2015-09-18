0ceb1e0a7ec58a50d2b4c0d3b7a31521b1f26546
Evan David Kristian <evan.kristian@mitrais.com>
2015-09-18 12:05:47 +0700

    build #9

--- Gruntfile.js
+++ Gruntfile.js
@@ -367,7 +367,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             '*.html',
-            'views/{,*/}*.html',
+            'views/**/*.html',
             'images/{,*/}*.{webp}',
             'styles/fonts/{,*/}*.*'
           ]
