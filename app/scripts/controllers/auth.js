c9f95e2a92b4bfcff8a7bade6bac00ce90ae1e4c
Evan David Kristian <evan.kristian@mitrais.com>
2015-09-18 11:59:51 +0700

    build #8

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -9,21 +9,27 @@
  */
 angular.module('erestoApp')
   .controller('AuthCtrl', function ($scope, $rootScope, User, localStorageService, $state, $timeout) {
+    $rootScope.controller = 'login';
     var token = localStorageService.get('token');
     if (token !== null){
       $state.go('app.dashboard');
     }
 
     $scope.doLogin = function(){
-      User.authenticate($scope.user)
-        .then(function(response){
-          localStorageService.set('token', response.token);
-          $timeout(function(){
-            $state.go('app.dashboard', { token: response.token });
-          }, 200);
-        }, function(){
-          $scope.user = {};
-          $rootScope.$broadcast('auth-error');
-        });
-    };
+          User.authenticate($scope.user)
+            .then(function(response){
+              if (response.role === 'superadmin' || response.role == 'owner'){
+                localStorageService.set('token', response.token);
+                $timeout(function(){
+                  $state.go('app.dashboard', { token: response.token });
+                }, 200);
+              }else{
+                $scope.signInError = "Only superadmin and owner can use this application.";
+              }
+              
+            }, function(res){
+              var message = res.data.message === 'User parameter is required' ? 'Email and password cannot be blank' : res.data.message;
+              $scope.signInError = "Oops, " + message;
+            });
+        };
   });
